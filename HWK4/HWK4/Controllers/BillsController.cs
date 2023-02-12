080af5ef334157acff1b96f68cfac568a6d162c2
Furqan <khanfa01@pfw.edu>
2023-02-11 20:10:43 -0500

    HWK4

---
+++ HWK4/HWK4/Controllers/BillsController.cs
@@ -0,0 +1,139 @@
+﻿using HWK4.Interfaces;
+using HWK4.Models;
+using HWK4.Repositories;
+using Microsoft.AspNetCore.Mvc;
+namespace HWK4.Controllers
+{
+        [ApiController]
+        [Route("[controller]")]
+
+        public class BillsController : ControllerBase
+        {
+
+            private readonly ILogger<BillsController> _logger;
+            private readonly IBillsRepository _billsRepository;
+
+            public BillsController(ILogger<BillsController> logger, IBillsRepository billsRepository)
+            {
+                _logger = logger;
+                _billsRepository = billsRepository;
+            }
+        /// <summary>
+        /// Get all the bills present in database
+        /// </summary>
+        /// <returns></returns>
+            [HttpGet]
+            [ProducesResponseType(200, Type = typeof(List<Bills>))]
+
+            public IActionResult GetBills()
+            {
+                _logger.Log(LogLevel.Information, "Get Bills");
+                return Ok(_billsRepository.GetBills());
+            } 
+            /// This method is a GET request that retrieves all bills from the repository.
+            
+
+
+            [HttpGet("{month}")] /// This method is a GET request that retrieves bills for a specific month
+            [ProducesResponseType(200, Type = typeof(Bills))] /// This attribute indicates that the expected response type is 200 OK and the returned data is of type Bills
+            [ProducesResponseType(404)] ///  if no bills are found for the specified month, a 404 Not Found response will be returned
+            
+            public IActionResult GetBill(String month)
+            {
+                _logger.Log(LogLevel.Information, "Get particular Bill");
+                Bills bill = _billsRepository.GetBill(month); /// Retrieves the bill for the specified month from the repository
+                if (bill == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    return Ok(bill);
+                }
+            }
+        /// <summary>
+        /// Creating new bills 
+        /// </summary>
+        /// <param name="bill">using the bill object</param>
+        /// <returns></returns>
+            [HttpPost]
+            [ProducesResponseType(200)]
+            [ProducesResponseType(400)]
+
+            public IActionResult CreateBills([FromBody] Bills bill)
+            {
+                if (bill == null)
+                {
+                    return BadRequest("Bills is null");
+                }
+                bool result = _billsRepository.CreateBills(bill);
+
+                if (result)
+                {
+                    return Ok("Successfully added");
+                }
+                else
+                {
+                    return BadRequest("Bill not added");
+                }
+            } 
+            
+
+        /// <summary>
+        /// Updating the bill data
+        /// </summary>
+        /// <param name="bill"> passing the bill object</param>
+        /// <returns></returns>
+            [HttpPut]
+            [ProducesResponseType(400)]
+            [ProducesResponseType(200)]
+            [ProducesResponseType(404)]
+
+            public IActionResult UpdateBill([FromBody] Bills bill)
+            {
+                if (bill == null)
+                {
+                    return BadRequest("Bill is null");
+                }
+                bool isUpdated = _billsRepository.UpdateBills(bill);
+
+                if (!isUpdated)
+                {
+                    return NotFound("No matching Bill");
+                }
+                else
+                {
+                    return Ok("Successfully updated");
+                }
+            } /// <summary>
+            /// This is a Put request to update the expense
+            /// </summary>
+                   
+            [HttpDelete]
+            public IActionResult DeleteBill([FromBody] Bills bill)
+            {
+                bool deleted = _billsRepository.DeleteBills(bill);
+                if (!deleted)
+                {
+                    return NotFound("No matching month");
+                }
+                else
+                {
+                    return Ok("Bill deleted");
+                }
+            } /// <summary>
+              /// Delete request for removing an entry.
+              /// </summary>
+            [HttpGet("Analyse")]
+            public IActionResult Analyse()
+            {
+                return Ok(_billsRepository.analyzeBill());
+            } //This is a get request to Analyse mean, median and mode.
+
+
+
+    }
+   }
+
+
+
---
+++ HWK4/HWK4/Interfaces/IBillsRepository.cs
@@ -0,0 +1,21 @@
+﻿using HWK4.Models;
+
+namespace HWK4.Interfaces
+
+{/// <summary>
+/// Declaration of all methods in the repository.
+/// </summary>
+    public interface IBillsRepository
+    {
+
+        ICollection<Bills> GetBills();
+
+        Bills GetBill(string Month);
+        bool BillsExists(string Month);
+        bool CreateBills(Bills bill);
+        bool UpdateBills(Bills bill);
+        bool DeleteBills(Bills bill);
+        Dictionary<string, dynamic> analyzeBill();
+        bool Save();
+    }
+}
