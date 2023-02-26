7a0a88efe091cbc13444c3a1605ca84fcfe76841
Furqan Khan <71590218+furqank8@users.noreply.github.com>
2023-02-25 22:40:26 -0500

    Add files via upload

---
+++ HW6/HWK4/HWK4/Models/Bills.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HWK4.Models
+{
+        public class BillsModel
+    {
+            [Key] //Month is the primary key
+            public string Month { get; set; }
+            public int Expense { get; set; }
+
+        public BillsModel(string month = "", int expense=0)
+            {
+                Month = month;
+                Expense = expense;
+
+            }//This is the constructor for the "Bills" class. It takes in two parameter "month" and "expense"
+        }
+    
+
+}
