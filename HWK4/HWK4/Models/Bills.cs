21a4b50ecedf3e017faab2cb042afc48ce570a5e
Furqan <khanfa01@pfw.edu>
2023-02-25 23:18:19 -0500

    Resolved Merge conflicts

--- HWK4/HWK4/Models/Bills.cs
+++ HWK4/HWK4/Models/Bills.cs
@@ -2,10 +2,7 @@
 
 namespace HWK4.Models
 {
-<<<<<<< HEAD
-=======
 
->>>>>>> b776ae7e5ece7d902e3d1fa7bc92c408f20110c0
         public class BillsModel
     {
             [Key] //Month is the primary key
@@ -13,18 +10,7 @@ namespace HWK4.Models
             public int Expense { get; set; }
 
         public BillsModel(string month = "", int expense=0)
-<<<<<<< HEAD
-=======
 
-        public class Bills
-        {
-            [Key] //Month is the primary key
-            public string Month { get; set; }
-            public int Expense { get; set; } = 0;
-
-        public Bills(string month, int expense)
-
->>>>>>> b776ae7e5ece7d902e3d1fa7bc92c408f20110c0
             {
                 Month = month;
                 Expense = expense;
